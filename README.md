606a86e446e10871f533dd3020554daf66b2c39c
antoandgar <antonioandgargiulo@gmail.com>
2024-02-14 18:49:34 +0100

    just playing with data

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # va-template
  Template for Visual Analytics projects
+
+```
+conda create -n visa
+conda activate visa
+conda install nodejs
+npm install
+npm run start
+```
--- src/index.js
+++ src/index.js
@@ -10,12 +10,91 @@ window.app = (new class {
   }
 
   async init() {
-    // await someFunctionThatLoadData
-    // loadedData = await d3.csvParse(await d3.FileAttachment("./Apartments.csv").text())
-    console.log(loadedData)
-    data = d3.table(loadedData)
-    console.log(loadedData.slice(1))
-    // Initialize your app
+    // Convert the array of arrays into an array of objects
+    this.data = loadedData.slice(1).map(row => ({
+      schoolId: +row[0],
+      monthlyCost: +row[1],
+      maxEnrollment: +row[2],
+      location: row[3],
+      buildingId: +row[4]
+    }))
+    console.log(this.data)
+
+    // Parse location from string to object
+    this.data.forEach(d => {
+      const [lon, lat] = d.location.match(/-?\d+\.\d+/g).map(Number)
+      d.location = { x: lon, y: lat }
+    })
+
+    // Call function to create visualization
+    this.createVisualization()
+  }
+
+  createVisualization() {
+    // Select the main container
+    const mainContainer = this.d3.select('#main')
+
+    // SVG dimensions
+    const margin = { top: 20, right: 20, bottom: 50, left: 50 }
+    const width = 800 - margin.left - margin.right
+    const height = 600 - margin.top - margin.bottom
+
+    // Create SVG
+    const svg = mainContainer.append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`)
+
+    // Scales
+    const xScale = this.d3.scaleLinear()
+      .domain([0, this.d3.max(this.data, d => d.monthlyCost) + 10])
+      .range([0, width])
+
+    const yScale = this.d3.scaleLinear()
+      .domain([0, this.d3.max(this.data, d => d.maxEnrollment) + 50])
+      .range([height, 0])
+
+    // Draw circles
+    svg.selectAll('circle')
+      .data(this.data)
+      .enter().append('circle')
+      .attr('cx', d => xScale(d.monthlyCost))
+      .attr('cy', d => yScale(d.maxEnrollment))
+      .attr('r', 6)
+
+    // Add labels
+    svg.selectAll('text')
+      .data(this.data)
+      .enter().append('text')
+      .attr('x', d => xScale(d.monthlyCost) + 10)
+      .attr('y', d => yScale(d.maxEnrollment) + 5)
+      .text(d => `School ${d.schoolId}`)
+
+    // Add axes
+    const xAxis = this.d3.axisBottom(xScale)
+    const yAxis = this.d3.axisLeft(yScale)
+
+    svg.append('g')
+      .attr('transform', `translate(0,${height})`)
+      .call(xAxis)
+
+    svg.append('g')
+      .call(yAxis)
+
+    // Add axis labels
+    svg.append('text')
+      .attr('transform', `translate(${width / 2},${height + margin.top + 20})`)
+      .style('text-anchor', 'middle')
+      .text('Monthly Cost')
+
+    svg.append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 0 - margin.left)
+      .attr('x', 0 - (height / 2))
+      .attr('dy', '1em')
+      .style('text-anchor', 'middle')
+      .text('Max Enrollment')
   }
 }())
 
