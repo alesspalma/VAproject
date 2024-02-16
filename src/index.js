913915a8b80793747fa459828de3e00b8c262962
antoandgar <antonioandgargiulo@gmail.com>
2024-02-16 17:23:07 +0100

    playing with grouping of data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import * as d3 from 'd3'
 import loadedData from '../data/Datasets/Attributes/Schools.csv'
 import './styles/index.scss'
 import movement from '../movement_split16.csv'
+import countBySectors from '../countBySectors.json'
 
 window.app = (new class {
   constructor() {
@@ -110,70 +111,116 @@ window.app = (new class {
     //   .style('text-anchor', 'middle')
     //   .text('Max Enrollment')
 
+    // MAP
     // Select the main container
-    const mainContainer = this.d3.select('#main')
+    // const mainContainer = this.d3.select('#main')
 
-    // SVG dimensions
+    // // SVG dimensions
+    // const margin = { top: 20, right: 20, bottom: 50, left: 50 }
+    // const width = 800 - margin.left - margin.right
+    // const height = 600 - margin.top - margin.bottom
+
+    // // Create SVG
+    // const svg = mainContainer.append('svg')
+    //   .attr('width', width + margin.left + margin.right)
+    //   .attr('height', height + margin.top + margin.bottom)
+    //   .append('g')
+    //   .attr('transform', `translate(${margin.left},${margin.top})`)
+
+    // // Scales
+    // const xScale = this.d3.scaleLinear()
+    //   .domain([this.d3.min(this.movement_data, d => d.x) - 50, this.d3.max(this.movement_data, d => d.x) + 50])
+    //   .range([0, width])
+
+    // const yScale = this.d3.scaleLinear()
+    //   .domain([this.d3.min(this.movement_data, d => d.y) - 50, this.d3.max(this.movement_data, d => d.y) + 50])
+    //   .range([height, 0])
+
+    // // Draw circles
+    // svg.selectAll('circle')
+    //   .data(this.movement_data)
+    //   .enter().append('circle')
+    //   .attr('cx', d => xScale(d.x))
+    //   .attr('cy', d => yScale(d.y))
+    //   .attr('r', 2)
+
+    // // Add axes
+    // const xAxis = this.d3.axisBottom(xScale)
+    // const yAxis = this.d3.axisLeft(yScale)
+
+    // svg.append('g')
+    //   .attr('transform', `translate(0,${height})`)
+    //   .call(xAxis)
+
+    // svg.append('g')
+    //   .call(yAxis)
+
+    // // Add axis labels
+    // svg.append('text')
+    //   .attr('transform', `translate(${width / 2},${height + margin.top + 20})`)
+    //   .style('text-anchor', 'middle')
+    //   .text('X')
+
+    // svg.append('text')
+    //   .attr('transform', 'rotate(-90)')
+    //   .attr('y', 0 - margin.left)
+    //   .attr('x', 0 - (height / 2))
+    //   .attr('dy', '1em')
+    //   .style('text-anchor', 'middle')
+    //   .text('Y')
+
+    // Define dimensions for the plot
+    const width = 1600
+    const height = 600
     const margin = { top: 20, right: 20, bottom: 50, left: 50 }
-    const width = 800 - margin.left - margin.right
-    const height = 600 - margin.top - margin.bottom
+    const innerWidth = width - margin.left - margin.right
+    const innerHeight = height - margin.top - margin.bottom
 
-    // Create SVG
-    const svg = mainContainer.append('svg')
+    // Append SVG
+    const svg = this.d3.select('#main').append('svg')
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`)
 
-    // Scales
-    const xScale = this.d3.scaleLinear()
-      .domain([this.d3.min(this.movement_data, d => d.x) - 50, this.d3.max(this.movement_data, d => d.x) + 50])
-      .range([0, width])
+    // Scale for x-axis
+    const xScale = d3.scaleBand()
+      .domain(countBySectors.map(d => d.id))
+      .range([margin.left, innerWidth])
+      .padding(0.1)
 
-    const yScale = this.d3.scaleLinear()
-      .domain([this.d3.min(this.movement_data, d => d.y) - 50, this.d3.max(this.movement_data, d => d.y) + 50])
-      .range([height, 0])
+    // Scale for y-axis
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(countBySectors, d => d.count)])
+      .nice()
+      .range([innerHeight, margin.top])
 
-    // Draw circles
-    svg.selectAll('circle')
-      .data(this.movement_data)
-      .enter().append('circle')
-      .attr('cx', d => xScale(d.x))
-      .attr('cy', d => yScale(d.y))
-      .attr('r', 2)
+    // Create the histogram bars
+    svg.selectAll('rect')
+      .data(countBySectors)
+      .enter().append('rect')
+      .attr('x', d => xScale(d.id))
+      .attr('y', d => yScale(d.count))
+      .attr('width', xScale.bandwidth())
+      .attr('height', d => innerHeight - yScale(d.count))
+      .attr('fill', 'white')
 
-    // Add labels
-    // svg.selectAll('text')
-    //   .data(this.movement_data)
-    //   .enter().append('text')
-    //   .attr('x', d => xScale(d.monthlyCost) + 10)
-    //   .attr('y', d => yScale(d.maxEnrollment) + 5)
-    //   .text(d => `School ${d.schoolId}`)
-
-    // Add axes
-    const xAxis = this.d3.axisBottom(xScale)
-    const yAxis = this.d3.axisLeft(yScale)
-
+    // Add x-axis
     svg.append('g')
-      .attr('transform', `translate(0,${height})`)
-      .call(xAxis)
+      .attr('transform', `translate(0,${innerHeight})`)
+      .call(d3.axisBottom(xScale).tickValues(xScale.domain().filter(function (d, i) {
+        return !(i % 10)
+      })))
+      .selectAll('text') // select all the text elements for x-axis
+      .style('text-anchor', 'end') // set text-anchor to end
+      .attr('dx', '-.8em') // set position of the labels
+      .attr('dy', '.15em') // set position of the labels
+      .attr('transform', 'rotate(-45)') // rotate labels by -45 degrees
 
+    // Add y-axis
     svg.append('g')
-      .call(yAxis)
-
-    // Add axis labels
-    svg.append('text')
-      .attr('transform', `translate(${width / 2},${height + margin.top + 20})`)
-      .style('text-anchor', 'middle')
-      .text('X')
-
-    svg.append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 0 - margin.left)
-      .attr('x', 0 - (height / 2))
-      .attr('dy', '1em')
-      .style('text-anchor', 'middle')
-      .text('Y')
+      .attr('transform', `translate(${margin.left},0)`)
+      .call(d3.axisLeft(yScale))
   }
 }())
 
